5b5931d5bf77ef05ba4ef0bba6f285c3c6fcef8d
Patrazzo <petarpetrov092@gmail.com>
2024-03-06 20:43:58 +0200

    SideBar Fix

--- client/src/components/Panel/panel.jsx
+++ client/src/components/Panel/panel.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { TodoColumn } from "../Columns/TodoColumn/todoColumn";
 import { InProgressColumn } from "../Columns/InProgressColumn/inProgressColumn";
 import { DoneColumn } from "../Columns/DoneColumn/doneColumn";
 import { Button } from "../Columns/Button/button";
 import Popup from "../Popup/popup";
 
-export const Panel = () => {
-  const [buttonPopup, setButtonPopup] = useState(false);
+export const Panel = ({ buttonPopup, setButtonPopup }) => {
   return (
     <div className="p-4 m-3 flex flex-col items-center justify-center z-0">
       <Button setButtonPopup={setButtonPopup} />
--- client/src/pages/Dashboard/dashboard.jsx
+++ client/src/pages/Dashboard/dashboard.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/Navbar/Header/header";
 import { Panel } from "../../components/Panel/panel";
 import { Footer } from "../../components/Navbar/Footer/footer";
 import { Sidebar } from "../../components/Navbar/Sidebar/sidebar";
+
 export const Dashboard = () => {
+  const [buttonPopup, setButtonPopup] = useState(false); // State management in Dashboard
+
   return (
     <div className="">
       <Header />
       <div className="flex flex-row justify-center h-auto min-h-screen phone:h-auto dark:bg-taskify-DarkBlue bg-taskify-lightBackground">
-        <Sidebar />
-        <Panel />
+        {buttonPopup ? " " : <Sidebar />}
+        <Panel buttonPopup={buttonPopup} setButtonPopup={setButtonPopup} />
       </div>
       <Footer></Footer>
     </div>
